83eab7f445acdb1d97ef7ef390d72f537af206f4
Arya Reais-Parsi <aryap@berkeley.edu>
2022-07-19 16:21:28 -0700

    update build, do not keep vlsir dir around

--- scripts/README.md
+++ scripts/README.md
@@ -3,6 +3,7 @@
 ## proto2cpp --layout
 
 TODO: depend on vlsir pypi bindings
+
 TODO: write this in rust to see what it's like
 
 ```
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm -rf vlsir
+mkdir -p vlsir
 pip3 install -r requirements.txt
-protoc -I=../vlsir --python_out vlsir ../vlsir/*.proto
+protoc -I=../vlsir --python_out vlsir ../vlsir/*.proto ../vlsir/**/*.proto
