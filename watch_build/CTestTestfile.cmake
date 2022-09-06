7933861ac7cb85adc1d3233deb9b12e4911b0650
Arya Reais-Parsi <himself@aryaparsi.com>
2022-09-07 08:53:23 +1200

    delete the build folder from git history. i should go back and change the commit actually

--- watch_build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arya/src/bfg
-# Build directory: /home/arya/src/bfg/watch_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/arya/src/bfg/watch_build/bfg_test[1]_include.cmake")
--- watch_build/view_script.py
+++
@@ -1,8 +0,0 @@
-import gdspy
-lib = gdspy.GdsLibrary(infile='library.gds')
-tops = lib.top_level()
-for cell in tops:
-  filename = f'library.{cell.name}.svg'
-  cell.write_svg(filename)
-  print(f'wrote {filename}')
-
