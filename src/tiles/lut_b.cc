252ac7bc155650320c432f1074f45f3d407bd3b4
Arya Reais-Parsi <himself@aryaparsi.com>
2024-11-15 13:13:26 -0800

    try narrowing lut by 2 more x_pitches

--- src/tiles/lut_b.cc
+++ src/tiles/lut_b.cc
@@ -219,7 +219,7 @@ bfg::Cell *LutB::GenerateIntoDatabase(const std::string &name) {
   mux_grid.set_template_cells(&mux_templates);
   // FIXME(aryap): This is a function of track pitch, really, not some number I
   // eyeballed.
-  mux_grid.set_horizontal_overlap(16 * met1_x_pitch);
+  mux_grid.set_horizontal_overlap(18 * met1_x_pitch);
   mux_grid.set_vertical_overlap(-2500);
   const std::vector<geometry::Instance*> &mux_order = mux_grid.InstantiateAll();
   mux_order_.insert(mux_order_.begin(), mux_order.begin(), mux_order.end());
