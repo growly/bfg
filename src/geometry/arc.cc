aebef5a15001e15ee0edbc7dd5f9055022df0c48
Arya Reais-Parsi <himself@aryaparsi.com>
2026-02-14 14:49:59 -0800

    fix Arc::IntersectingPoints method

--- src/geometry/arc.cc
+++ src/geometry/arc.cc
@@ -29,7 +29,7 @@ std::vector<Point> Arc::IntersectingPoints(const Line &line) const {
   double y_c = centre_.y();
   double r = radius_;
  
-  // If vertical, we substitute x = k into the circule equation:
+  // If vertical, we substitute x = k into the circle equation:
   //
   //                (k - x_c)^2 + (y - y_c)^2 = r^2 
   //                     y^2 - 2y_c*y + y_c^2 = r^2 - (k - x_c)^2
@@ -47,12 +47,12 @@ std::vector<Point> Arc::IntersectingPoints(const Line &line) const {
   if (line.IsVertical()) {
     double k = line.start().x();
     double B = -2.0 * y_c;
-    double C = y_c * y_c - r * r + (k + x_c) * (k + x_c);
+    double C = y_c * y_c - r * r + (k - x_c) * (k - x_c);
     std::vector<double> y_values = Utility::SolveQuadraticReal(1.0, B, C);
 
     std::vector<Point> result;
     for (double y_value : y_values) {
-      result.emplace_back(y_value, k);
+      result.emplace_back(k, y_value);
     }
     return result;
   }
@@ -65,23 +65,33 @@ std::vector<Point> Arc::IntersectingPoints(const Line &line) const {
   //
   // Substituting y into the circle equation,
   //
-  //                           (x - x_c)^2 + (mx + (c - y_c))^2 = r^2
-  //       x^2 -2x_c*x  + m^2*x^2 + 2m(c - y_c)*x + (c - y_c)^2 = r^2
-  // x^2 -2x_c*x  + m^2*x^2 + 2m(c - y_c)*x + (c - y_c)^2 - r^2 = 0
-  //    (1 + m^2)x + (2m(c - y_c) - 2x_c)*x + (c - y_c)^2 - r^2 = 0
+  //                                (x - x_c)^2 + (mx + (c - y_c))^2 = r^2
+  //     x^2 -2x_c*x + x_c^2 + m^2*x^2 + 2m(c - y_c)*x + (c - y_c)^2 = r^2
+  // x^2 -2x_c*x + x_c^2 m^2*x^2 + 2m(c - y_c)*x + (c - y_c)^2 - r^2 = 0
+  // (1 + m^2)x + (2m(c - y_c) - 2x_c)*x + x_c^2 + (c - y_c)^2 - r^2 = 0
   //
   // This is a quadratic in x,
   //                           Ax^2 + Bx + C = 0
   //
   // with A =  (1 + m^2)
   //      B = 2(mc - my_c - x_c)
-  //      C = (c - y_c)^2 - r^2
-  return {};
+  //      C = x_c^2 + (c - y_c)^2 - r^2
+  double A = 1 + m*m;
+  double B = 2*(m*(c - y_c) - x_c);
+  double C = x_c * x_c + (c - y_c) * (c - y_c) - r * r;
+  std::vector<double> x_values = Utility::SolveQuadraticReal(A, B, C);
+
+  std::vector<Point> result;
+  for (double x_value : x_values) {
+    result.emplace_back(x_value, m*x_value + c);
+  }
+  return result;
 }
 
 bool Arc::InArcBounds(const Point &point) const {
   double angle_rad = std::atan2(
-      static_cast<double>(point.y()), static_cast<double>(point.x()));
+      static_cast<double>(point.y() - centre_.y()),
+      static_cast<double>(point.x() - centre_.x()));
 
   if (angle_rad < 0) {
     angle_rad += 2 * Radian::kPi;
