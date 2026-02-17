9da4244d59250a089d2ec9c42b4cb9e7ddec0f09
Arya Reais-Parsi <himself@aryaparsi.com>
2026-02-17 10:07:12 -0800

    TODOs toDONE

--- src/tiles/carry1.h
+++ src/tiles/carry1.h
@@ -66,9 +66,6 @@ class Carry1 : public Tile {
         parameters_(parameters) {
   }
 
-  // TODO(aryap): Don't think I need the circuit argument. They have to be
-  // connected together all at once by a caller.
-  // TODO(aryap): Don't think I need the layout argument either.
   geometry::Instance* AddConfigMemory(RowGuide *row) const;
   geometry::Instance* AddGenerateSelectMux(RowGuide *row) const;
   geometry::Instance* AddSumXor(RowGuide *row) const;
