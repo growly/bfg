0388aa36b45f3a8de39f9440536cfc3fb4839f8f
Arya Reais-Parsi <himself@aryaparsi.com>
2026-07-18 21:38:51 -0700

    claude can draw a transistor

--- src/atoms/layout_demo.h
+++ src/atoms/layout_demo.h
@@ -12,6 +12,13 @@ namespace bfg {
 
 class DesignDatabase;
 
+// NOTE TO READER: This class was generated entirely by Claude. I prompted the
+// whole thing. I told it to use examples from the rest of the codebase and it
+// seems to have correctly copy-pasted the right stuff. In particular, I told
+// it to use Sky130SimpleTransistor, which basically does this with a lot of
+// extra fluff. Anyway that means that, too, can use a coding agent to help you
+// build VLSI layout!
+
 namespace atoms {
 
 // Minimal layout generator, used as a starting point / example for building
@@ -19,6 +26,12 @@ namespace atoms {
 class LayoutDemo : public Atom {
  public:
   struct Parameters : public Sky130Parameters {
+    // Transistor dimensions, in nanometres. Note that in layout the FET "width"
+    // runs along the diffusion (drawn vertically here), while the "length" is
+    // the width of the poly gate that crosses it.
+    uint64_t width_nm = 500;
+    uint64_t length_nm = 170;
+
     void ToProto(proto::parameters::LayoutDemo *pb) const {}
     void FromProto(const proto::parameters::LayoutDemo &pb) {}
   };
